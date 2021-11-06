d8f1e5418a2258145e8ecdfa867b895d486cf8d0
Tomachi [ICHIGO] <lego@aol.jp>
2021-11-07 02:41:57 +0900

    fix: 受付終了 + 未提出 + 他の人の課題提出内容が見える状態の場合にエラーになる不具合の修正

--- manaba/__init__.py
+++ manaba/__init__.py
@@ -1208,12 +1208,15 @@ class Manaba:
                 raise ManabaInternalError(
                     "get_task_status return None (" + statuses[0].strip() + ")")
 
-            your_status = get_your_status(statuses[1].strip())
-            if your_status is None:
-                your_status = get_your_status(statuses[2].strip())
+            if "まだ提出は可能です" in statuses[1]:  # 未提出 & ※遅延として取り扱われますが、まだ提出は可能です。
+                your_status = ManabaTaskYourStatusFlag.UNSUBMITTED
+            else:
+                your_status = get_your_status(statuses[1].strip())
                 if your_status is None:
-                    raise ManabaInternalError(
-                        "your_status return None (" + statuses[1].strip() + " | " + statuses[2].strip() + ")")
+                    your_status = get_your_status(statuses[2].strip())
+                    if your_status is None:
+                        raise ManabaInternalError(
+                            "your_status return None (" + statuses[1].strip() + " | " + statuses[2].strip() + ")")
 
             return ManabaTaskStatus(task_status, your_status)
 
