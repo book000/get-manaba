a8a0e54465c19113c64c52f69abd4e45f1629a9e
Tomachi [ICHIGO] <lego@aol.jp>
2021-12-11 19:28:00 +0900

    chore: version 2.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='get-manaba',
-    version='2.0.9',
+    version='2.0.10',
     packages=setuptools.find_packages(),
     install_requires=["beautifulsoup4", "requests", "html5lib"],
     url='https://github.com/book000/get-manaba',
