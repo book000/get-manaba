181a4b1e948325e14a08e3e158a3c0da2524a75e
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-01-19 13:10:18 +0900

    chore: v2.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='get-manaba',
-    version='2.3.1',
+    version='2.3.2',
     packages=setuptools.find_packages(),
     install_requires=["beautifulsoup4", "requests", "html5lib"],
     url='https://github.com/book000/get-manaba',
